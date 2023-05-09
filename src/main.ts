1549e0ba37f258daca9fc59e2ad2e75b52127b58
zack zheng <zack_zhengxiyun@163.com>
2023-05-09 19:49:04 +0800

    LICENSE

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,8 @@
+// Copyright (c) 2023 Zack Zheng<zack_zhengxiyun@163.com>
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
 import App from '@/App'
 import router from '@/router/index'
 
